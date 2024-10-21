d5d1bcc55d6f7fa75e69dcafb41036513c00c656
Thomas McCarthy-Howe <ghostofbasho@gmail.com>
2024-10-21 23:15:04 +0000

    Refactor follower.py to improve egress flushing and logging

--- server/follower.py
+++ server/follower.py
@@ -5,6 +5,7 @@ from lib.error_tracking import init_error_tracker
 from config import Configuration
 import requests
 import threading
+import time
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -17,6 +18,31 @@ imported_modules = {}
 
 r = redis_mgr.get_client()
 
+def flush_egress(follower):
+    # Estimate the rate of flushing the egress list
+    # to be something like a Nyquist frequency
+    if follower["flushing_rate"]:
+        time.sleep(follower["flushing_rate"])
+    else:
+        time.sleep(follower["pulling_interval"] / 2)
+    endpoint = (
+        follower["url"]
+        + "/vcon/egress?egress_list="
+        + follower["egress_list"]
+        + "&limit=" 
+        + str(follower["fetch_vcon_limit"])
+    ) 
+    headers = {
+        "Content-Type": "application/json",
+        "x-conserver-api-token": follower["auth_token"],
+    }
+    while True:
+        response = requests.request("GET", endpoint, headers=headers)
+        vcon_uuids = response.json()
+        if not vcon_uuids:
+            break
+        print(f"Flushing egress list of vcons: {len(vcon_uuids)}")
+        time.sleep(5)
 
 def follower_function(follower):
     logger.info("Starting follower function")
@@ -29,11 +55,11 @@ def follower_function(follower):
     )
     headers = {
         "Content-Type": "application/json",
-        "Authorization": "Bearer " + follower["auth_token"],
+        "x-conserver-api-token": follower["auth_token"],
     }
     response = requests.request("GET", endpoint, headers=headers)
     vcons_to_process = response.json()
-    logger.info("VCONs to process: %s", vcons_to_process)
+    logger.info(f"VCONs to process: {len(vcons_to_process)}")
 
     if not vcons_to_process:
         return
@@ -42,15 +68,14 @@ def follower_function(follower):
         endpoint = follower["url"] + "/vcon/" + vcon_id
         headers = {
             "Content-Type": "application/json",
-            "Authorization": "Bearer " + follower["auth_token"],
+            "x-conserver-api-token": follower["auth_token"],
         }
         response = requests.request("GET", endpoint, headers=headers)
         if response.status_code == 404:
-            logger.error("Failed to get VCON: %s", vcon_id)
+            logger.error(f"Failed to get VCON: {vcon_id}")
+            logger.error(response.json())
             continue
         vcon = response.json()
-        # logger.info("VCON ID: %s", vcon_id)
-        # logger.info("VCON: %s", vcon)
         redis_mgr.set_key(f"vcon:{vcon_id}", vcon)
         r.lpush(follower["follower_ingress_list"], vcon_id)
 
@@ -64,11 +89,16 @@ def start_followers():
     logger.info("Starting follower loop")
     global config
     config = Configuration.get_followers()
-    # ingress_chain_map = get_ingress_chain_map()
-    # all_ingress_lists = list(ingress_chain_map.keys())
 
     for follower_name, follower in config.items():
         logger.info("Follower: ", follower)
+        
+        # If the flush_first flag is set, make sure you flush the 
+        # vCons ids from the egress redist list
+        if follower.get("flush_first", False):
+            logger.info("Flushing egress list of vcons")
+            flush_egress(follower)
+            
         repeat_function(follower["pulling_interval"], follower_function, follower)
 
 
